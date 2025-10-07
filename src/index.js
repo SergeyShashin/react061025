b8208d0b81bf21dfce43d5de56e84a0e57385cd3
SergeyShashin <s9997910@yandex.ru>
2025-10-07 07:46:02 +0300

    React061025 lesson1 done.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@
 // console.log('Welcome World!)');
 // console.log(user.sayWelcome());
 
-import React from "react";
 // import ReactDom from "react-dom";
+import React from "react";
 import { createRoot } from 'react-dom/client';
 
-const element = React.createElement(
-  'h1',
-  { className: 'react-welcome' },
-  'Привет, Реакт работает!)',
-);
+// const element = React.createElement(
+//   'h1',
+//   { className: 'react-welcome' },
+//   'Привет, Реакт работает!)',
+// );
+
+// const element = <h1 className="welcome-react">Привет! JSX тоже работает!</h1>;
+
+const messages = ['Привет, друг!', 'Как дела?', 'Как погода?', 'Как настроение?'];
+
+const Message = props => <li>{props.msg}</li>
+const Messages = props => props.messages.map((msg, idx) => <ul><Message msg={msg} key={idx} /></ul>);
 
 // ReactDom.render(
 //   element,
 //   document.getElementById('root')
 // );
+
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+// root.render(element);
+// root.render(<Message msg={'Привет, компонент работает!)'} />);
+root.render(<Messages messages={messages} />);
