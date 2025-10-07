7deb132b7881f77895026c8b140828c1278dbcb5
SergeyShashin <s9997910@yandex.ru>
2025-10-07 14:37:45 +0300

    React161025. Lesson2 updated.

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -8,7 +8,6 @@ export class MessagesList extends Component {
   }
 
   render() {
-    console.log(this.props.messages);
     return (
       <div>
         {this.props.messages.map((message, idx) => <Message key={idx} message={message} />)}
--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -15,7 +15,34 @@ export class Messenger extends Component {
     this.state = {
       messages: [{ author: 'Автор', text: 'Привет, друг!)' }]
     };
+    this.interval = 0;
 
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      let randIdx = Math.floor(Math.random() * messages.length);
+
+      this.setState({
+        messages: this.state.messages.concat({ author: 'Автор', text: messages[randIdx] })
+      })
+
+    }, 2000)
+  }
+
+  componentDidUpdate() {
+    const { messages } = this.state;
+    const { author } = messages[messages.length - 1];
+    if (author === 'Автор') {
+      setTimeout(() => this.setState({
+        messages: messages.concat({ author: 'Автоответчик', text: `Здравствуйте, ${author}!) Сообщение получено.` })
+      }), 1000);
+
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render() {
