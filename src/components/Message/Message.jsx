2ad9512c4e5d74c3ad47339ef0d108e04fd56015
SergeyShashin <s9997910@yandex.ru>
2025-10-12 14:07:30 +0300

    React061025 lesson3 updated.

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,18 +2,33 @@ import './Message.scss';
 
 import React, { Component } from 'react';
 
+import classNames from 'classnames';
+
 export class Message extends Component {
   constructor(props) {
     super(props);
   }
 
+  get direction() {
+    const { author } = this.props.message;
+    return author === 'Автоответчик' ? 'end' : 'start'
+  }
+
   render() {
     const { author, text } = this.props.message;
+    const classes = classNames('message', {
+      'message-author': author !== 'Автоответчик',
+      'message-autoAnswer': author === 'Автоответчик',
+    });
     return (
-      <div className='message'>
+      // <div className='message' style={{
+      //   alignSelf: `flex-${this.direction}`
+      // }
+      // }>
+      <div className={classes}>
         <p>{author}:</p>
-        <p>{text}</p>
-      </div>
+        <p className='sender'>{text}</p>
+      </div >
 
     )
   }
