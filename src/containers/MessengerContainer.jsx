d7bf1de2604d74b976ef7109468c09524fb98a11
SergeyShashin <s9997910@yandex.ru>
2025-11-02 16:14:01 +0300

    React061025. Lesson5 uodated.

--- src/containers/MessengerContainer.jsx
+++ src/containers/MessengerContainer.jsx
@@ -1,11 +1,32 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { Messenger } from 'components/Messenger';
+import { load } from 'actions/chats';
 
 class MessengerContainer extends PureComponent {
+  componentDidMount() {
+    const { chats } = this.props;
+    loadChats();
+  }
   render() {
+    const { chats } = this.props;
     return (
-      <Messenger />
+      <Messenger chats={chats} />
     )
   }
 }
+
+function mapStateToProps(state, ownProps) {
+  return {
+    chats: state.chats.get('entries').toJS()
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    load: () => dispatch(load())
+  }
+
+}
+
+export const MessengerRedux = connect(mapStateToProps, mapDispatchToProps)(MessengerContainer);
